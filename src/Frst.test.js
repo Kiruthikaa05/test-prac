a3d69223ee9501fd6e767f9c7e4812e21eedc514
Kiruthikaa05 <105360432+Kiruthikaa05@users.noreply.github.com>
2022-06-08 08:46:36 +0530

    Update Frst.test.js

--- src/Frst.test.js
+++ src/Frst.test.js
@@ -4,3 +4,10 @@ describe("addition",()=>{
     expect(add(100000,19999)).toBe(119999)
 })
 });
+const obj=require ('./Obj)
+describe("object",()=>{
+  it('Run',()=>{
+     let data=obj()
+     expect (data).toEqual({Name:"vega",Age:5})   
+})
+})
